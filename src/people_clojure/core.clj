080bc5f83051ed368b28024ddcabccb20950b82b
MattBrown <litteriver1337@gmail.com>
2015-11-24 11:36:18 -0500

    LOLOL IM SO IN HEAVEN!

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,7 +1,25 @@
 (ns people-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [people (slurp "people.csv")                           ;lol slurp reads the csv file
+        people (str/split-lines people)         ;str is the clojure.string as an alias and /split-lines splits people
+        people (map (fn [line]
+                      (str/split line #","))                ;comma seperated values
+                    people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (interleave header line))
+                    people)
+        people (map (fn [line]
+                      (apply hash-map line))
+                    people)
+        people (walk/keywordize-keys people)
+        people (filter (fn [line]
+                         (= "Brazil" (:country line)))
+                       people)]
+    (spit "filtered_peope.edn"
+          (pr-str people))))
