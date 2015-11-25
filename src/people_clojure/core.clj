d9fe5280c1262a2cd9c0868c45a900b0f8332f2c
MattBrown <litteriver1337@gmail.com>
2015-11-25 10:57:49 -0500

    added html and some links

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,9 +1,10 @@
 (ns people-clojure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [ring.adapter.jetty :as jetty]
+            [hiccup.core :as h])
   (:gen-class))
-
-(defn -main [& args]
+(defn read-people []
   (let [people (slurp "people.csv")                           ;lol slurp reads the csv file
         people (str/split-lines people)         ;str is the clojure.string as an alias and /split-lines splits people
         people (map (fn [line]
@@ -21,5 +22,31 @@
         people (filter (fn [line]
                          (= "Brazil" (:country line)))
                        people)]
-    (spit "filtered_peope.edn"
-          (pr-str people))))
+    #_(spit "filtered_peope.edn"
+          (pr-str people))
+    people))
+
+(defn people-html []
+  (let [people (read-people)]
+    (map (fn [line]
+           [:p
+            (str (:first_name line)
+                 ""
+                 (:last_name line))])
+         people)))
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                      [:a {:href "http://www.theironyard.com"}
+                       "The Iron Yard"]
+                      [:br]
+                      [:b {:href "http://www.riseofagon.com"}
+                       "Rise Of Agon"]
+                      [:br]
+                      (people-html)]])})
+
+(defn -main [& args]
+  (jetty/run-jetty #'handler {:port 3000 :join? false}))    ;#' makes refresh dynamic and uses latest version of that function
